25fcc32b0cce797c983872b12bee91889cc0ecbb
Ariel Ayala <Ariel.Ayala@quisitive.com>
2023-11-10 23:34:08 -0300

    Invert String

---
+++ JuniorTechnicalTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Crear una instancia de la clase Program
+        Program program = new Program();
+
+        
+        program.InvertString();
+    }
+
+    /// <summary>
+    /// It reverses a given string and prints it to the console.
+    /// </summary>
+    private void InvertString()
+    {
+        string text = "example";
+        string textResult = "";
+
+        for (int i = text.Length - 1; i >= 0; i--)
+        {
+            textResult += text[i];
+        }
+
+        Console.WriteLine(textResult);
+    }
+}

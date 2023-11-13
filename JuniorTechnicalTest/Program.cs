d75f2957b23f1a63968144baa4988acf67b5b140
Ariel Ayala <Ariel.Ayala@quisitive.com>
2023-11-13 15:02:26 -0300

    Count character using linq

--- JuniorTechnicalTest/Program.cs
+++ JuniorTechnicalTest/Program.cs
@@ -8,7 +8,7 @@ class Program
         Program program = new Program();
 
         
-        program.CountCharacter();
+        program.CountCharacterUsingLinq();
     }
 
     /// <summary>
@@ -61,5 +61,20 @@ class Program
         }
 
         Console.WriteLine($"character: {character} is {counter} times");
+    }
+
+    /// <summary>
+    /// Counts the occurrences of a specific character in a given string using LINQ.
+    /// </summary>
+    /// <remarks>
+    /// This method leverages LINQ (Language Integrated Query) to count the number of times
+    /// the specified character appears in the provided string.
+    /// </remarks>
+    private void CountCharacterUsingLinq()
+    {
+        string text = "asdkkfdkcnjdiqyewrqotinkbzncbnvbm";
+        char character = 'n';
+        int count = text.Where(x => x == character).Count();
+        Console.WriteLine($"character: {character} is {count} times");
     }
 }
